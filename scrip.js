475f08a6738a2202f51fb3e1eae3aefdffbae41d
tipalsyahhh <tipalsyah123@gmail.com>
2024-08-07 11:02:51 +0700

    profile

---
+++ scrip.js
@@ -0,0 +1,20 @@
+    VANTA.BIRDS({
+      el: "body",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      backgroundColor: 0xffffff,
+      color1: 0x000,
+      color2: 0x000,
+      birdSize: 1.30,
+      wingSpan: 18.00,
+      speedLimit: 8.00,
+      separation: 27.00,
+      alignment: 17.00,
+      cohesion: 26.00,
+      quantity: 3.00
+    })
